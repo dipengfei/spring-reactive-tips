783b79e51d0070d964cc767fd16e6c0499cbba54
dipengfei <pengfei.di@rakuten.com>
2020-05-03 12:31:03 +0800

    initial commit

---
+++ src/main/java/me/danielpf/springreactivetips/SpringReactiveTipsApplication.java
@@ -0,0 +1,13 @@
+package me.danielpf.springreactivetips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveTipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveTipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/danielpf/springreactivetips/SpringReactiveTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.danielpf.springreactivetips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
