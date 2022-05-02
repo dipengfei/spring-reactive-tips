1f09993994c945beed1cd3c374504d8d1ba31b9e
dipengfei <pengfei.di@rakuten.com>
2022-05-02 23:52:18 +0800

    upgrade spring-boot version number.

--- src/main/java/me/danielpf/springreactivetips/SpringReactiveTipsApplication.java
+++ src/main/java/me/danielpf/springreactivetips/SpringReactiveTipsApplication.java
@@ -1,13 +1,39 @@
 package me.danielpf.springreactivetips;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SpringReactiveTipsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringReactiveTipsApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner multiplicationTablesRunnerWithStream() {
+        return args ->
+                IntStream.rangeClosed(1, 9)
+                         .boxed()
+                         .flatMap(m -> IntStream.rangeClosed(1, m)
+                                                .boxed()
+                                                .map(n -> String.format("%d * %d = %d%s", m, n, m * n, m.equals(n) ? "\n" : "\t")))
+                         .forEach(System.out::print);
+    }
+
+    @Bean
+    ApplicationRunner multiplicationTablesRunnerWithFlux() {
+        return args ->
+                Flux.range(1, 9)
+                    .concatMap(m -> Flux.range(1, m)
+                                        .map(n -> String.format("%d * %d = %d%s", m, n, m * n, m.equals(n) ? "\n" : "\t")))
+                    .subscribe(System.out::print);
+
     }
 
 }
