fe6468b6ddf4133c049d3021c5316c15ca12d0e1
dipengfei <pengfei.di@rakuten.com>
2022-05-04 23:34:57 +0800

    upgrade spring-boot version number.

--- src/main/java/me/danielpf/springreactivetips/SpringReactiveTipsApplication.java
+++ src/main/java/me/danielpf/springreactivetips/SpringReactiveTipsApplication.java
@@ -1,14 +1,19 @@
 package me.danielpf.springreactivetips;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
+@Slf4j
 public class SpringReactiveTipsApplication {
 
     public static void main(String[] args) {
@@ -34,6 +39,25 @@ public class SpringReactiveTipsApplication {
                                         .map(n -> String.format("%d * %d = %d%s", m, n, m * n, m.equals(n) ? "\n" : "\t")))
                     .subscribe(System.out::print);
 
+    }
+
+
+    @Bean
+    ApplicationRunner nonInterferenceRunner() {
+        return args -> {
+            var list = new ArrayList<>(Arrays.asList("a", "b", "c"));
+
+            try {
+                list.stream().peek(s -> list.add("d")).forEach(System.out::println);
+            } catch (Exception e) {
+                System.out.println("ConcurrentModificationException occurred here");
+            }
+
+            var set = ConcurrentHashMap.newKeySet();
+            set.addAll(Arrays.asList("A", "B", "C"));
+            set.stream().peek(s -> set.add("D")).forEach(System.out::println);
+
+        };
     }
 
 }
